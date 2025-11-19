f64bcd78ca088a81f6331db88bacda49236538d5
NQAnh-HCMUS <nqanh211@clc.fitus.edu.vn>
2025-11-19 10:58:33 +0700

    organize part 2

---
+++ src/admin/Graph.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Graph {
+    public static void chooseYear() {
+        JFrame frame = new JFrame("LogIn Log - Admin Only");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+
+        JPanel root = new JPanel(new BorderLayout(6,6));
+
+        JTextArea listArea = new JTextArea();
+        JScrollPane scroll = new JScrollPane(listArea);
+        root.add(scroll, BorderLayout.CENTER);
+
+        JPanel inputPanel = new JPanel(new BorderLayout(6,6));
+        JTextField inputField = new JTextField();
+        JButton sendBtn = new JButton("Search");
+        inputPanel.add(inputField, BorderLayout.CENTER);
+        inputPanel.add(sendBtn, BorderLayout.EAST);
+
+        inputField.addActionListener(e -> sendBtn.doClick());
+
+        root.add(inputPanel, BorderLayout.SOUTH);
+
+        frame.setContentPane(root);
+        frame.setVisible(true);
+    }
+}
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import src.admin.*;
+
 import javax.swing.*;
 import java.awt.*;
 
